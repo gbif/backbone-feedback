12dbe6011b9e569bef2ee6d1354c609d219c8496
John Waller <ftw712@ku.dk>
2025-03-11 16:42:42 +0100

    run label maker script

--- scripts/create_github_label.sh
+++ scripts/create_github_label.sh
@@ -1,8 +1,6 @@
 
 #!/bin/bash
 
-starting_dir=$(pwd)
-
 tsv_file="report.tsv"
 header_skipped=true
 while IFS=$'\t' read -r issue status type; do
@@ -10,11 +8,13 @@ while IFS=$'\t' read -r issue status type; do
         header_skipped=false
         continue
     fi
-    cd /mnt/c/Users/ftw712/Desktop/scripts/shell/bb/backbone-feedback
+ 
     status=${status//\"/}  # Unquote the status variable
     issue=${issue//\"/}  # Unquote the issue variable
+ 
     echo $status
     echo $issue
+
     # remove all previous labels 
     gh issue edit $issue --remove-label 'autocheck - issue open on xRelease'
     gh issue edit $issue --remove-label 'autocheck - issue closed on xRelease'
@@ -33,22 +33,8 @@ while IFS=$'\t' read -r issue status type; do
         gh issue edit $issue --add-label 'autocheck - status unknown on xRelease'
     fi
 
-    cd "$starting_dir"
-
 done < "$tsv_file"
 
-# gh issue edit $issue --add-label "autocheck - closed on xRelease"
-# gh issue edit $issue --add-label "autocheck - open on xRelease"
-cd "$starting_dir"
-
-
-# cd /mnt/c/Users/ftw712/Desktop/scripts/shell/bb/backbone-feedback
-# gh issue edit $issue --add-label "occurrenceID - large change in record counts"
-# cd "$starting_dir"
-
-# issue=$(gh issue list --search "is:issue is:open label:$uuid"|
-# awk '{print $1}')
-# cd "$starting_dir"
 
 
 
