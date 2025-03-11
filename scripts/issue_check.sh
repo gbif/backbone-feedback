895c47291c1dd328a015a1618543897f121b7de2
John Waller <ftw712@ku.dk>
2025-03-11 16:19:40 +0100

    checkig if report.tsv exists

--- scripts/issue_check.sh
+++ scripts/issue_check.sh
@@ -28,6 +28,11 @@ do
     echo "Processing issue: $issue"
     COMMENTS=$(curl -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/gbif/backbone-feedback/issues/$issue/comments)
     
+    if [ -z "$COMMENTS" ]; then
+        echo "Error: No comments received for issue $issue"
+        continue
+    fi
+
     if ! echo "$COMMENTS" | jq empty; then
         echo "Error: Invalid JSON received for issue $issue"
         continue
